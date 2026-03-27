5e2f4ee2f168080ce544b112d118d9d49f838fb8
hashir <hashir704x@gmail.com>
2026-03-27 14:59:17 +0500

    working on string problems

--- file1.ts
+++ file1.ts
@@ -20,7 +20,6 @@ function problem1x() {
   console.log(total);
 }
 
-
 function problem1() {
   const array = [2, 4, 6, 5, 8];
   let i = array.length - 1,
@@ -32,3 +31,34 @@ function problem1() {
   }
   console.log(total);
 }
+
+function problem2() {
+  // reverse a string
+  const str = "Netsol";
+  let tempStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    tempStr = tempStr + str[i];
+  }
+
+  console.log(tempStr);
+}
+
+function problem2WithTwoPointer() {
+  // reverse a string
+  const str = "racecar";
+  let i = 0,
+    j = str.length - 1,
+    isPalindrome = true;
+  while (i < j) {
+    if (str[i] !== str[j]) {
+      isPalindrome = false;
+      break;
+    }
+    i++;
+    j--;
+  }
+  if (isPalindrome) console.log("Yes, palindrome");
+  else console.log("No, palindrome");
+}
+
+problem2WithTwoPointer();
